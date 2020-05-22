cb62091b4be49bda32a582bf8fc135b3471a78a8
ShaneMit <shanemit@cox.net>
2020-05-21 19:08:31 -0700

    get /notes now returns notes.html

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
+app.get('/notes', (req, res) => {
+  res.sendFile(join(__dirname, 'public/notes.html'))
+})
+
 
 
 app.listen(3000, () => console.log('http://localhost:3000'))
